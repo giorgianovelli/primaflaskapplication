62a2bfe3b8fd3202eedc9f85e3ca51279357b310
giorgianovelli <32876200+giorgianovelli@users.noreply.github.com>
2019-11-26 16:09:42 +0100

    aggiunta collegamento file html e css

--- applicazioneFlask/applicazione.py
+++ applicazioneFlask/applicazione.py
@@ -1,18 +1,29 @@
-from flask import Flask
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+# @app.route('/')
+# def index_page_landing():
+#     return '<h1>Welcome</h1>'
+
 @app.route('/')
 def index_page_landing():
-    return '<h1>Welcome</h1>'
+     return "Method used: %s" %request.method
+
+@app.route("/provametodi", methods=['GET', 'POST'])
+def provametodi():
+    if request.method == 'POST':
+        return "You are using POST"
+    else:
+        return "You are using GET"
 
 @app.route('/secondpage')
 def secondpage():
     return '<h2>prova seconda pagina</h2>'
 
-@app.route('/profile/<username>')
-def profile(username):
-    return "hello %s" % username
+@app.route('/profile/<name>')
+def profile(name):
+    return render_template("profile.html", name=name)
 
 @app.route('/post/<int:post_id>')
 def post(post_id):
