108748a02303e82be054505fa1c46c1219b9bf12
giorgianovelli <32876200+giorgianovelli@users.noreply.github.com>
2019-11-26 15:03:09 +0100

    aggiunta della seconda pagina

--- applicazioneFlask/applicazione.py
+++ applicazioneFlask/applicazione.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index_page_landing():
     return '<h1>Welcome</h1>'
 
+@app.route('/secondpage')
+def secondpage():
+    return '<h2>prova seconda pagina</h2>'
+
 
 if __name__ == "__main__":
     app.run()
